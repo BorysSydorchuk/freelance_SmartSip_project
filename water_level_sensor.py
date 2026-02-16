5887dc29338b6bf51c35df34dfe872b3cc62e068
Siebe <siebe.Verlinden1@student.kuleuven.be>
2026-02-16 15:59:28 +0100

    test

--- water_level_sensor.py
+++ water_level_sensor.py
@@ -8,6 +8,8 @@ S1 = 6
 S2 = 5
 Z  = 26
 
+water_level_list = []
+
 # Select pins are outputs
 GPIO.setup(S0, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(S1, GPIO.OUT, initial=GPIO.LOW)
@@ -24,10 +26,22 @@ def select_channel(ch):
     GPIO.output(S1, int(bits[1]))
     GPIO.output(S2, int(bits[0]))  # MSB
 
-def read_channel(ch)
+def read_channel(ch):
     select_channel(ch)
     time.sleep(0.0005)  # small settling time (0.5 ms); can be lower
     return GPIO.input(Z)
 
+
 while True:
+    for i in range(7):
+        water_level_list.append(read_channel(i))
+        time.sleep(0.01)
+    
+    water_level = water_level_list.count(1)
+    print (water_level)
+    print (water_level_list)
+    water_level_list = []
+    time.sleep(0.1)
+
+    
     
