6b0dae442f6e5ff4bcb0c7b65d59260049b3f381
Devroede Mathias <mathias.devroede@student.kuleuven.be>
2026-03-13 08:49:33 +0100

    fixed print statement if desired temp is not between cold temp tank and hot temp tank

--- Raspi1/calorimetry.py
+++ Raspi1/calorimetry.py
@@ -3,10 +3,11 @@
 def calculateWater(volume_bottle,tempHotTank,tempColdTank,desiredTemp):#this is when we assume there is no water yet in the bottle (so no starting temp in bottle)
     massWaterFullbottle=volume_bottle*10    # f.e 70 cl is 700g 
     if not tempColdTank<=desiredTemp<=tempHotTank: 
-       print("Error, choose a temperature between"+tempColdTank+"°C and "+ tempHotTank+"°C" )     
-    else:       #formula is derived from m_cold*C_water*T_cold+m_hot*C_water*T_hot=(m_cold+mhot)*C_water*T_desired C is constant so you can drop it 
+       print("Error, choose a temperature between"+str(tempColdTank)+"°C and "+ str(tempHotTank)+"°C" )     
+       return None
+          #formula is derived from m_cold*C_water*T_cold+m_hot*C_water*T_hot=(m_cold+mhot)*C_water*T_desired C is constant so you can drop it 
          
-         mass_cold= massWaterFullbottle*(tempHotTank-desiredTemp)/(tempHotTank-tempColdTank)   
+    mass_cold= massWaterFullbottle*(tempHotTank-desiredTemp)/(tempHotTank-tempColdTank)   
 
 
     return mass_cold                                
